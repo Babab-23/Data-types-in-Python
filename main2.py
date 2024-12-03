769f5a48876d6c086832e7d360ccd718c804792c
Babab-23 <adrianidialu@gmail.com>
2024-12-03 21:10:40 +0100

    second commit

---
+++ main2.py
@@ -0,0 +1,27 @@
+# Assigning Different Variables
+name="Adrian"
+age="15"
+is_student= True
+weight=38.5
+
+# Pinting Different Variables and their Data types
+print("Name:",name)
+print("Data type of name is",type(name))
+
+print("Age:",age)
+print("Data type of age is",type(age))
+
+print("is_student:",is_student)
+print("Data type of is_student is",type(is_student))
+
+print("Weight is:",weight)
+print("Data type of weight is",type(weight))
+
+# Type Casting to convert the datatype of variables
+print("\n Type Casting...")
+age=str(age)
+print(age)
+print("Data type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is",type(weight))
